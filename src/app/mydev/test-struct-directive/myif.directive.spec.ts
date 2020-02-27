e88a4d97864f02c51b7ad040c72f24295371c244
Alexey808 <laiten808@gmail.com>
2020-02-27 22:40:01 +0400

    -

--- src/app/mydev/test-struct-directive/myif.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyifDirective } from './myif.directive';
-
-describe('MyifDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyifDirective();
-    expect(directive).toBeTruthy();
-  });
-});

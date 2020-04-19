4a1f507e02f4974bc19b7fc50958e414d07b252b
Alexey Svetinskiy <laiten808@gmail.com>
2020-04-19 19:48:21 +0400

    -

--- src/app/dev/ng-animate/test-ng-animate/test-ng-animate.component.ts
+++ src/app/dev/ng-animate/test-ng-animate/test-ng-animate.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {state, style, transition, trigger, useAnimation} from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger, useAnimation } from '@angular/animations';
 import {bounce, bounceIn, pulse, shake, zoomIn, zoomOut} from 'ng-animate';
+
+const an = [
+  // void => *
+  transition(':enter', useAnimation(bounceIn, {
+    params: { timing: 0.5 }
+  })),
+  // * => void
+  transition(':leave', useAnimation(zoomOut, {
+    params: { timing: 0.5 }
+  })),
+  // transition('* <=> *', useAnimation(bounceIn, {
+  //   params: { timing: 0.5 }
+  // })),
+];
+
+const pulseAnimate = [
+  transition('* => true', [
+    useAnimation(pulse, {
+      params: { timing: 0.5 }
+    })
+  ])
+];
 
 @Component({
   selector: 'app-test-ng-animate',
   templateUrl: './test-ng-animate.component.html',
   styleUrls: ['./test-ng-animate.component.scss'],
   animations: [
-    trigger('mybounce', [
-      // void => *
-      transition(':enter', useAnimation(bounceIn, {
-        params: { timing: 0.5 }
-      })),
-      // * => void
-      transition(':leave', useAnimation(zoomOut, {
-        params: { timing: 0.5 }
-      })),
-      // transition('* <=> *', useAnimation(bounceIn, {
-      //   params: { timing: 0.5 }
-      // })),
-    ])
+    trigger('mybounce', an),
+    trigger('pulseTrigger', pulseAnimate),
   ],
 })
 export class TestNgAnimateComponent implements OnInit {
   bool = true;
-
+  items = [{num: 1, checked: false}, {num: 2, checked: true}, {num: 3, checked: false}];
 
   constructor() { }
 
@@ -33,5 +44,13 @@ export class TestNgAnimateComponent implements OnInit {
 
   onClick() {
     this.bool = !this.bool;
+  }
+
+  changeChecked(item) {
+    const updateItems = this.items.map((x) => {
+      x.checked = item.num === x.num;
+      return x;
+    });
+    this.items = updateItems;
   }
 }

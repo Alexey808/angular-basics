436fc3e5749e1d60067b0ec8719bfd784fdb8d79
alexey.svetinskiy <alexey808sv@gmail.com>
2020-08-14 01:09:11 +0400

    -

--- src/app/+dev/testing-marble/testing-marble.component.spec.ts
+++ src/app/+dev/testing-marble/testing-marble.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestingMarbleComponent } from './testing-marble.component';
+import { cold, getTestScheduler } from 'jasmine-marbles';
+import { TestingMarbleService } from './testing-marble.service';
+import { AppComponent } from '../../app.component';
+import { By } from '@angular/platform-browser';
 
 describe('TestingMarbleComponent', () => {
-  let component: TestingMarbleComponent;
-  let fixture: ComponentFixture<TestingMarbleComponent>;
+  // let component: TestingMarbleComponent;
+  // let fixture: ComponentFixture<TestingMarbleComponent>;
+  let service: any; //: TestingMarbleService;
 
   beforeEach(async(() => {
+    service = jasmine.createSpy('TestingMarbleService'); // 1 имитируем сервис
+    service.getUsers = cold('a-b-c', { a: 'Mike', b: 'Flo', c: 'Rolf' }); // 2 создание холодной подписки
+
     TestBed.configureTestingModule({
-      declarations: [ TestingMarbleComponent ]
+      declarations: [ TestingMarbleComponent ],
+      providers: [{provide: TestingMarbleService, useValue: service}]
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestingMarbleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('should be created', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges(); // trigger change detection
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    getTestScheduler().flush(); // flush the observable
+    fixture.detectChanges(); // trigger change detection again
+
+    const liElements = fixture.debugElement.queryAll(By.css('.user'));
+    expect(liElements.length).toBe(3);
+
+    expect(liElements[0].nativeElement.innerText).toBe('Mike');
+    expect(liElements[1].nativeElement.innerText).toBe('Flo');
+    expect(liElements[2].nativeElement.innerText).toBe('Rolf');
   });
 });
--- src/app/+dev/testing-marble/testing-marble.service.spec.ts
+++ src/app/+dev/testing-marble/testing-marble.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TestingMarbleService } from './testing-marble.service';
+import { cold, getTestScheduler } from 'jasmine-marbles';
+import { AppComponent } from '../../app.component';
+import { By } from '@angular/platform-browser';
 
 describe('TestingMarbleService', () => {
   let service: TestingMarbleService;
